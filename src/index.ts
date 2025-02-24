b8c5542d434d52f67acba5983c28985e98895a49
Andrii <jitelgor@gmail.com>
2025-02-24 20:01:57 +0000

    1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 export { default as MatchMedia, type MatchMediaProps } from "./MatchMedia";
 export { default as useMatchMedia } from "./useMatchMedia";
-export {
-  default as MatchMediaHydrationProvider,
-  type MatchMediaHydrationContext,
-} from "./MatchMediaHydrationProvider";
+export { default as MatchMediaHydrationProvider } from "./MatchMediaHydrationProvider";
 export {
   default as configureNodeEnv,
   type MediaQueryEnvironmentConfig,
+  type MatchMediaHydrationContext,
 } from "./configureNodeEnv";
